22f176a71255b34d45e32f474924448fafe9513b
LongLH <d@dapps.me>
2018-04-06 21:45:38 +0700

    improve UX

--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,7 @@ import boolean from 'boolean'
 import delay from 'delay'
 import ect from 'ect'
 import express from 'express'
+import http from 'http'
 import mkdirp from 'mkdirp'
 import mime from 'mime'
 import mimeMatch from 'mime-match'
@@ -9,6 +10,7 @@ import path from 'path'
 import phantom from 'phantom'
 import pretty from 'pretty-bytes'
 import shortHash from 'shorthash'
+import socket from 'socket.io'
 import request from 'superagent'
 import { URL } from 'url'
 
@@ -29,9 +31,14 @@ const engine = ect({
   ext: '.ect'
 })
 
+const reports = {}
+
 app.set('view engine', 'ect')
 app.engine('ect', engine.render)
 app.set('views', viewDir)
+
+app.use('/s', express.static(path.join(__dirname, '../../screenshot')))
+app.use('/libs', express.static(path.join(__dirname, '../../node_modules')))
 
 app.use((req, res, next) => {
   const timeout = setTimeout(() => {
@@ -137,221 +144,259 @@ app.get('/', (req, res, next) => {
   res.render('index')
 })
 
-app.get('/analyze', [
-  (req, res, next) => {
-      (async () => {
-        req._instance = await phantom.create()
+app.get('/reports/:tag', (req, res, next) => {
+  const report = reports[req.params.tag]
 
-        next()
-      })()
+  if (!report) {
+    return res.redirect('/')
+  }
 
-      res.on('finish', async () => {
-        console.log('Clear phantom...')
+  res.render('analyze', report)
+})
 
-        await req._instance.exit()
+const log = (requester) => {
+  let anchor
 
-        console.log('Clear phantom... done!')
-      })
-  },
-  (req, res, next) => {
-    if (!req.query.url) {
-      return res.sendStatus(400)
+  return (info, finish) => {
+    if (!finish) {
+      anchor = Date.now()
     }
 
-    (async () => {
-      const page = await req._instance.createPage([
-        '--ignore-ssl-errors=yes',
-        '--debug=true'
-      ], {
-        logLevel: 'debug'
-      })
+    console.log(info, finish ? Date.now() - anchor : '')
 
-      const all = {}
-      const images = {}
-      const css = []
-      const js = []
-      const viewportSize = {
-        // width: 375,
-        width: parseInt(req.query.w || 1280, 10),
-        height: parseInt(req.query.h || 900, 10)
-      }
-
-      const ua = boolean(req.query.m) ? MOBILE_UA : DESKTOP_UA
-
-      await page.property('viewportSize', viewportSize)
-      await page.setting('userAgent', ua)
-      await page.on('onResourceReceived', (requestData) => {
-        const { url } = requestData
-        const decodedUrl = decodeURIComponent(url)
-
-        all[decodedUrl] = requestData
-
-        const type = requestData.contentType ||
-          mime.getType(new URL(decodedUrl).pathname)
-
-        if (!type) {
-          console.log(`Strange request ${requestData.url}`)
-
-          return
-        }
-
-        if (mimeMatch(type, 'image/*')) {
-          // console.info('Requesting', url)
-          images[decodedUrl] = requestData
-        } else if (mimeMatch(type, 'text/css')) {
-          css.push(url)
-        }
-      })
-
-      console.time('PageResponse')
-      const url = new URL(req.query.url)
-
-      console.log(`GET ${url.toString()} ${ua}`)
-
-      const status = await page.open(url.toString())
-
-      console.timeEnd('PageResponse')
-
-      console.time('Capture')
-
-      await page.evaluate(function() {
-        const style = document.createElement('style')
-        const text = document.createTextNode('body { background: #fff }')
-
-        style.setAttribute('type', 'text/css')
-        style.appendChild(text)
-        document.head.insertBefore(style, document.head.firstChild)
-
-        window.scrollTo(0, document.body.scrollHeight)
-      })
-
-      const timeout = delay.reject(5e3, 'timeout')
-
-      try {
-        await waitForReady(page, timeout)
-
-        await timeout
-      } catch (e) {
-        if (e instanceof delay.CancelError) {
-          console.log('Finish waiting for rendering')
-        } else {
-          console.log('Cannot wait anymore, capture anyway')
-        }
-      }
-
-      if (req.query.delay) {
-        await delay(parseInt(req.query.delay, 10))
-      }
-
-      const normalize = normalizeUrl(url.protocol, url.hostname)
-
-      // report
-      const imgTags = (await page.evaluate(function() {
-        const imgs = document.querySelectorAll('img')
-
-        return [].slice.call(imgs).map(function(img) {
-          return {
-            natural:{
-              width: img.naturalWidth,
-              height: img.naturalHeight
-            },
-            displayed: {
-              width: img.clientWidth,
-              height: img.clientHeight
-            },
-            src: img.getAttribute('src')
-          }
-        })
-      })).filter(Boolean).filter(img => {
-        return img.src && img.natural.width > 0 && img.natural.height > 0 && img.displayed.width > 0 && img.displayed.height > 0
-      }).map(img => {
-        const u = normalize(img.src.trim())
-
-        const data = images[u]
-
-        if (!data) {
-          console.log('====')
-          console.log(u)
-          console.log(img.src)
-          console.log('====')
-
-          return null
-        }
-
-        data.imgTag = true
-        const size = parseInt(getImageSize(data), 10)
-
-        return {
-          ...img,
-          ...data,
-          size,
-          prettySize: pretty(size || 0),
-          canOptimized: img.natural.width > img.displayed.width || img.natural.height > img.displayed.height
-        }
-      }).filter(Boolean)
-
-      const screenshot = shortHash.unique(req.query.url) + '.jpeg'
-
-      mkdirp.sync(path.dirname(screenshot))
-
-      await page.render(path.join(screenshotDir, screenshot), {
-        quality: 50
-      })
-
-      console.timeEnd('Capture')
-
-      const cssImages = Object.values(images)
-        .filter(img => !img.imgTag)
-        .map(img => {
-          const size = parseInt(getImageSize(img), 10)
-
-          return {
-            ...img,
-            size,
-            prettySize: pretty(size || 0),
-            natural: { width: 0, height: 0 },
-            displayed: { width: 0, height: 0 }
-          }
-        })
-
-      const imgs = await optimize([...imgTags, ...cssImages])
-
-      // return res.json({
-      //   imgs,
-      //   images
-      // })
-
-      // res.sendFile(screenshot)
-      // res.redirect(`/s/${screenshot}`)
-      const totalSize = imgs.reduce((size, imgs) => size + (imgs.size || 0), 0)
-      const totalOptimizedSize = imgs.reduce((size, imgs) => size + (imgs.optimizedSize || 0), 0)
-
-      imgs.forEach(img => {
-        img.percent = img.size ?
-          (img.optimizedSize / img.size) * 100 : 100
-      })
-
-      res.render('analyze', {
-        all,
-        totalSize,
-        totalOptimizedSize,
-        screenshot: `/s/${screenshot}`,
-        imgTags: imgs,
-        imageData: images,
-        prettyTotalSize: pretty(totalSize || 0),
-        prettyTotalOptimizedSize: pretty(totalOptimizedSize || 0),
-        percent: totalSize ?
-         (totalOptimizedSize / totalSize) * 100 : 100
-      })
-    })()
-    .catch(error => {
-      if (!res.headersSent) {
-        res.sendStatus(500)
-      }
+    io.to(requester).emit('analyze_progress', {
+      info: info,
+      time: finish ? Date.now() - anchor : null
     })
   }
-])
+}
 
-app.use('/s', express.static(path.join(__dirname, '../../screenshot')))
+const analyze = async (data, requester) => {
+  const updateProgress = log(requester)
 
-app.listen(3005, () => console.log('App started at: 3005'))
+  const reportTag = shortHash.unique(data.url)
+
+  const report = reports[reportTag]
+
+  if (report && report.created + (5 * 60e3) > Date.now()) {
+    updateProgress('Cache hit')
+
+    io.to(requester).emit('analyze_complete', {
+      reportLink: `/reports/${reportTag}`
+    })
+
+    return
+  }
+
+  let anchor
+
+  const instance = await phantom.create([
+    '--ignore-ssl-errors=yes'
+  ])
+
+  const page = await instance.createPage()
+
+  const url = new URL(data.url)
+  const viewportSize = {
+    // width: 375,
+    width: parseInt(data.w || 1280, 10),
+    height: parseInt(data.h || 900, 10)
+  }
+  const ua = boolean(data.m) ? MOBILE_UA : DESKTOP_UA
+
+  await page.property('viewportSize', viewportSize)
+  await page.setting('userAgent', ua)
+
+  const all = {}
+  const images = {}
+
+  await page.on('onResourceReceived', requestData => {
+    const { url } = requestData
+    const decodedUrl = decodeURIComponent(url)
+
+    all[decodedUrl] = requestData
+
+    const type = requestData.contentType ||
+      mime.getType(new URL(decodedUrl).pathname)
+
+    if (!type) {
+      console.log(`Strange request ${requestData.url}`)
+
+      return
+    }
+
+    if (mimeMatch(type, 'image/*')) {
+      // console.info('Requesting', url)
+      images[decodedUrl] = requestData
+    }
+  })
+
+  updateProgress(`GET ${url.toString()} ...`)
+
+  const status = await page.open(url.toString())
+
+  updateProgress(`GET ${url.toString()} ${status}`, true)
+
+  updateProgress(`Render...`)
+
+  await page.evaluate(function() {
+    const style = document.createElement('style')
+    const text = document.createTextNode('body { background: #fff }')
+
+    style.setAttribute('type', 'text/css')
+    style.appendChild(text)
+    document.head.insertBefore(style, document.head.firstChild)
+
+    window.scrollTo(0, document.body.scrollHeight)
+  })
+
+  const timeout = delay.reject(5e3, 'timeout')
+
+  try {
+    await waitForReady(page, timeout)
+
+    await timeout
+  } catch (e) {
+    if (e instanceof delay.CancelError) {
+      updateProgress('Render... done', true)
+    } else {
+      updateProgress('Render takes longer than 5s... done', true)
+    }
+  }
+
+  if (data.delay) {
+    await delay(parseInt(data.delay, 10))
+  }
+
+  const screenshot = `${reportTag}.jpeg`
+
+  mkdirp.sync(path.dirname(screenshot))
+
+  await page.render(path.join(screenshotDir, screenshot), {
+    quality: 50
+  })
+
+  updateProgress(`Captured /s/${screenshot}`)
+
+  updateProgress(`Inspect DOM...`)
+
+  const normalize = normalizeUrl(url.protocol, url.hostname)
+
+  // report
+  const imgTags = (await page.evaluate(function() {
+    const imgs = document.querySelectorAll('img')
+
+    return [].slice.call(imgs).map(function(img) {
+      return {
+        natural:{
+          width: img.naturalWidth,
+          height: img.naturalHeight
+        },
+        displayed: {
+          width: img.clientWidth,
+          height: img.clientHeight
+        },
+        src: img.getAttribute('src')
+      }
+    })
+  })).filter(Boolean).filter(img => {
+    return img.src && img.natural.width > 0 && img.natural.height > 0 && img.displayed.width > 0 && img.displayed.height > 0
+  }).map(img => {
+    const u = normalize(img.src.trim())
+
+    const data = images[u]
+
+    if (!data) {
+      console.log('====')
+      console.log(u)
+      console.log(img.src)
+      console.log('====')
+
+      return null
+    }
+
+    data.imgTag = true
+    const size = parseInt(getImageSize(data), 10)
+
+    return {
+      ...img,
+      ...data,
+      size,
+      prettySize: pretty(size || 0),
+      canOptimized: img.natural.width > img.displayed.width || img.natural.height > img.displayed.height
+    }
+  }).filter(Boolean)
+
+  const cssImages = Object.values(images)
+    .filter(img => !img.imgTag)
+    .map(img => {
+      const size = parseInt(getImageSize(img), 10)
+
+      return {
+        ...img,
+        size,
+        prettySize: pretty(size || 0),
+        natural: { width: 0, height: 0 },
+        displayed: { width: 0, height: 0 }
+      }
+    })
+
+  updateProgress(`Inspect DOM... done`, true)
+
+  updateProgress(`Optimize...`)
+
+  const imgs = await optimize([...imgTags, ...cssImages])
+
+  updateProgress(`Optimize... done`, true)
+
+  updateProgress(`Summary...`)
+
+  const totalSize = imgs.reduce((size, imgs) => size + (imgs.size || 0), 0)
+  const totalOptimizedSize = imgs.reduce((size, imgs) => size + (imgs.optimizedSize || 0), 0)
+
+  imgs.forEach(img => {
+    img.percent = img.size ?
+      (img.optimizedSize / img.size) * 100 : 100
+  })
+
+  updateProgress(`Summary... done`, true)
+
+  reports[reportTag] = {
+    all,
+    totalSize,
+    totalOptimizedSize,
+    screenshot: `/s/${screenshot}`,
+    imgTags: imgs,
+    imageData: images,
+    prettyTotalSize: pretty(totalSize || 0),
+    prettyTotalOptimizedSize: pretty(totalOptimizedSize || 0),
+    percent: totalSize ?
+     (totalOptimizedSize / totalSize) * 100 : 100,
+    created: Date.now()
+  }
+
+  updateProgress(`Completed`)
+
+  io.to(requester).emit('analyze_complete', {
+    reportLink: `/reports/${reportTag}`
+  })
+
+  await instance.exit()
+}
+
+const server = http.Server(app)
+const io = socket(server)
+
+io.on('connection', async (socket) => {
+  console.log('[socket.io] an user connected')
+
+  socket.on('disconnect', async () => {
+    console.log('[socket.io] the user disconnected')
+  })
+
+  socket.on('request_analyze', async (msg) => {
+    analyze(msg, socket.id)
+  })
+})
+
+server.listen(3005, () => console.log('App started at: 3005'))
