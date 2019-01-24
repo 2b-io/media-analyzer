17f7529c174580fe8a1053742afc79f5901d25a4
LongLH <d@dapps.me>
2019-01-24 10:47:51 +0700

    add css/stylus loader

--- internals/webpack.config.babel.js
+++ internals/webpack.config.babel.js
@@ -2,12 +2,15 @@ import glob from 'glob'
 import path from 'path'
 
 import CleanWebpackPlugin from 'clean-webpack-plugin'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import OptimizeCssAssetsPlugin from 'optimize-css-assets-webpack-plugin'
 import WebpackAssetsManifest from 'webpack-assets-manifest'
 
 const rootDir = path.join(__dirname, '..')
 const resourceDir = path.join(rootDir, './src/resources')
 const outDir = path.join(rootDir, 'data/dist/assets')
 
+const devMode = process.env.NODE_ENV !== 'production'
 const cdn = 'http://d-14:3006'
 
 export default {
@@ -32,6 +35,86 @@ export default {
       output: path.join(outDir, '../manifest.json'),
       publicPath: `${ cdn }/assets/`,
       writeToDisk: true
-    })
-  ]
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[hash:6].css'
+    }),
+    new OptimizeCssAssetsPlugin()
+  ],
+  resolve: {
+    extensions: [ '.css', '.js', '.styl' ],
+    modules: [
+      'node_modules',
+      'src/resources'
+    ]
+  },
+  module: {
+    rules: [ {
+      test: /\.m?js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            '@babel/preset-env'
+          ],
+          plugins: [
+            '@babel/plugin-proposal-object-rest-spread',
+            '@babel/plugin-transform-runtime'
+          ]
+        }
+      }
+    }, {
+      test: /\.css/,
+      use: [ {
+        loader: devMode ?
+          'style-loader' :
+          MiniCssExtractPlugin.loader
+      }, {
+        loader: 'css-loader'
+      } ]
+    }, {
+      test: /\.styl$/,
+      use: [ {
+        loader: devMode ?
+          'style-loader' :
+          MiniCssExtractPlugin.loader
+      }, {
+        loader: 'css-loader'
+      }, {
+        loader: 'stylus-loader',
+        options: {
+          import: [
+            '~kouto-swiss/index.styl'
+          ],
+          preferPathResolver: 'webpack'
+        }
+      } ]
+    }, {
+      test: /\.(ico|jpg|png|gif|svg|bmp|webp|mp4)$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            name: 'img/[name].[hash:6].[ext]',
+            publicPath: `${ cdn }/assets`,
+            emitFile: true
+          }
+        }
+      ]
+    },
+    {
+      test: /\.(ttf|eot|woff|woff2)$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            name: 'fonts/[name].[hash:6].[ext]',
+            publicPath: `${ cdn }/assets`,
+            emitFile: true
+          }
+        }
+      ]
+    } ]
+  }
 }
