602a7f449a2b9371c29c2598e27df01c42fa6153
cuongtv <truongcuong90@gmail.com>
2019-01-28 15:08:23 +0700

    Move screenshot to index

--- src/server/services/analyze/index.js
+++ src/server/services/analyze/index.js
@@ -1,12 +1,13 @@
-import delay from 'delay'
 import mkdirp from 'mkdirp'
 import path from 'path'
 
 import getImageTag from 'services/analyze/get-image-tag'
 import initBrowser from 'services/analyze/init-browser'
 import initPage from 'services/analyze/init-page'
-import loadPage from 'services/analyze/load-page'
 import intepreceptionRequest from 'services/analyze/intepreception-request'
+import metrics from 'services/analyze/metrics'
+import loadPage from 'services/analyze/load-page'
+import screenshot from 'services/analyze/screenshot'
 
 const LOAD_PAGE_NUMBER = 3
 const screenshotDir = path.join(__dirname, '../../../../screenshot')
@@ -23,9 +24,11 @@ const analyze = async (params, progress) => {
   const originPage = await initPage(browser, params)
 
   for (var i = 0; i < LOAD_PAGE_NUMBER; i++) {
-    await loadPage(originPage, params, progress, screenshotDir, i)
+    await loadPage(originPage, params, progress)
 
-    await delay(500)
+    await screenshot(originPage, `${ params.tag }-optimize`, progress, screenshotDir, i)
+
+    await metrics(originPage)
   }
 
   const originImgTags = await getImageTag(originPage)
@@ -40,7 +43,9 @@ const analyze = async (params, progress) => {
   for (var i = 0; i < LOAD_PAGE_NUMBER; i++) {
     await loadPage(optimizePage, params, progress, screenshotDir, i)
 
-    await delay(500)
+    await screenshot(optimizePage, `${ params.tag }-optimize`, progress, screenshotDir, i)
+
+    await metrics(optimizePage)
   }
 
   await optimizePage.Close()
--- src/server/services/analyze/load-page.js
+++ src/server/services/analyze/load-page.js
@@ -1,17 +1,10 @@
-import metrics from 'services/analyze/metrics'
-import screenshot from 'services/analyze/screenshot'
-
-const loadPage = async (page, params, progress, screenshotDir, screenshotIndex) => {
+const loadPage = async (page, params, progress) => {
   progress(`[${params.mode}] GET ${params.url} ...`)
 
   await page.goto(params.url, {
     waitUntil: params.mode || 'load',
     timeout: 2 * 60 * 1000 // 2 minutes
   })
-
-  await screenshot(page, `${ params.tag }-optimize`, progress, screenshotDir, screenshotIndex)
-
-  return await metrics(page)
 }
 
 export default loadPage
