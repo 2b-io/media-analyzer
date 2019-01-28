6b42888f6ef149d240f5f58e7c91dc3b4109ebc5
cuongtv <truongcuong90@gmail.com>
2019-01-28 14:53:28 +0700

    Remove config dev

--- internals/config.dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  server: {
-    port: 3009,
-    url: 'http://localhost:3009'
-  },
-  endpoint: 'https://viscous-shelter-71.media-cdn.io',
-  mongodb: 'mongodb://mongo/media-analyze'
-}
