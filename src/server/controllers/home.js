95b7f77cdbcff985ee7cf954395e93fb28f05c09
LongLH <d@dapps.me>
2019-02-13 11:03:57 +0700

    remove fetching recent reports when visit home page

--- src/server/controllers/home.js
+++ src/server/controllers/home.js
@@ -1,25 +1,8 @@
-import reportService from 'services/report'
-
 export default {
   get: [
-    async (req, res, next) => {
-      const recentReports = await reportService.list({
-        error: {
-          $ne: true
-        },
-        desktop: {
-          $ne: null
-        },
-        mobile: {
-          $ne: null
-        },
-        finish: true
-      }, null, {
-        limit: 20
-      })
-
+    (req, res, next) => {
       res.render('pages/home', {
-        reports: recentReports
+        reports: []
       })
     }
   ]
