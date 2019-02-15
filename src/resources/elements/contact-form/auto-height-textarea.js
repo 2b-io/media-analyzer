b27ce9a242691d2bf9510ea01cae5ca221b31364
LongLH <d@dapps.me>
2019-02-15 13:42:12 +0700

    fix textarea autofocus and flick

--- src/resources/elements/contact-form/auto-height-textarea.js
+++ src/resources/elements/contact-form/auto-height-textarea.js
@@ -1,29 +1,14 @@
-const observe = (element, event, handler) => {
-  element.addEventListener(event, handler, false)
-}
-
 window.addEventListener('load', () => {
   const textAreas = document.querySelectorAll('.input-line.multi-line')
 
   Array.from(textAreas).forEach((textArea) => {
     const resize = () => {
-      textArea.style.height = 'auto'
+      textArea.style.height = 0 //'auto'
       textArea.style.height = `${ textArea.scrollHeight }px`
     }
 
-    const delayedResize = () => {
-      setTimeout(resize, 0)
-    }
+    textArea.addEventListener('input', resize, false)
 
-    observe(textArea, 'change', resize)
-    observe(textArea, 'cut', delayedResize)
-    observe(textArea, 'paste', delayedResize)
-    observe(textArea, 'drop', delayedResize)
-    observe(textArea, 'keydown', delayedResize)
-
-    // init
-    textArea.focus()
-    textArea.select()
     resize()
   })
 })
