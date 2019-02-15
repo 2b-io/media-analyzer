31e9f6c83d013bea4cde9f54d1ffbc861a512272
cuongtv <truongcuong90@gmail.com>
2019-02-15 19:31:31 +0700

    Add google capcha

--- src/server/controllers/contact.js
+++ src/server/controllers/contact.js
@@ -1,14 +1,17 @@
 import bodyParser from 'body-parser'
+import { BAD_REQUEST } from 'http-status-codes'
 import joi from 'joi'
 
 import contact from 'services/contact'
+import recaptcha from 'services/google-recaptcha'
 
 const SCHEMA = joi.object().keys({
   name: joi.string().trim().required(),
   email: joi.string().lowercase().email().required(),
   company: joi.string().allow('').trim(),
   content: joi.string().allow('').trim(),
-  phone: joi.number().allow('')
+  phone: joi.number().allow(''),
+  token: joi.string().trim()
 })
 
 export default {
@@ -17,11 +20,27 @@ export default {
     async (req, res, next) => {
       try {
         const body = req.body
+        const token = body['g-recaptcha-response']
+
+        if (!token) {
+          return res.sendStatus(BAD_REQUEST)
+        }
+
+        delete body['g-recaptcha-response']
+
+        const params = { ...body, token }
         const values = await joi.validate(body, SCHEMA)
+
+        const result = await recaptcha(token)
+
+        if (!result.success) {
+          return
+        }
+
         await contact.update(values)
         res.render('pages/contact')
       } catch (e) {
-        return
+        return res.sendStatus(BAD_REQUEST)
       }
     }
   ]
