47e86c92bcf639a7bbc8445de4951b34607f755f
cuongtv <truongcuong90@gmail.com>
2019-02-19 18:36:03 +0700

    Remove console.log

--- src/server/services/analyzer/generate-har.js
+++ src/server/services/analyzer/generate-har.js
@@ -2,7 +2,7 @@ import PuppeteerHar from 'puppeteer-har'
 
 const generateHar = async (page, harName) => {
   const har = new PuppeteerHar(page)
-  console.log('harName', harName)
+
   await har.start({ path: harName })
 
   await har.stop()
