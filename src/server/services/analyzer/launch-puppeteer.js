5a49cff21c0b6185a1ac53b05a7f04979d6ca5cb
cuongtv <truongcuong90@gmail.com>
2019-02-19 11:28:35 +0700

    Update parallel analyzer

---
+++ src/server/services/analyzer/launch-puppeteer.js
@@ -0,0 +1,297 @@
+import fetch from 'node-fetch'
+import puppeteer from 'puppeteer'
+import path from 'path'
+
+import config from 'infrastructure/config'
+import loadPage from 'services/analyzer/load-page'
+import normalizeUrl from 'services/analyzer/normalize-url'
+import setRequestImage from 'services/analyzer/set-request-image'
+
+import reportService from 'services/report'
+
+const initBrowser = async (params) => {
+  const browser = await puppeteer.launch({
+    headless: true,
+    executablePath: 'google-chrome',
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--window-size=1440,900',
+      '--proxy-server="direct://"',
+      '--proxy-bypass-list=*',
+      '--enable-features=NetworkService'
+    ],
+    ignoreHTTPSErrors: true
+  })
+
+  return browser
+}
+
+const launchPuppeteer = async (params) => {
+  const {
+    url,
+    identifier,
+    timeout
+  } = params
+
+  const state = {
+    inspect: false,
+    images: {}
+  }
+
+  const browser = await initBrowser(params)
+
+  try {
+    // create a new incognito browser context
+    const incognitoContext = await browser.createIncognitoBrowserContext()
+    const originDesktopPage = await incognitoContext.newPage()
+
+    try {
+      // load origin page
+      await reportService.updateProgress(identifier, 'Load origin desktop page...')
+      console.log('Load origin desktop page')
+      console.time('Load origin desktop page')
+
+      // set request interception
+      await originDesktopPage.setRequestInterception(true)
+
+      originDesktopPage.on('request', (request) => {
+        if (!state.inspect && request.resourceType() === 'image') {
+          const url = request.url()
+
+          state.images[url] = { url }
+        }
+
+        request.continue()
+      })
+
+      const result = await loadPage(originDesktopPage, {
+        url,
+        timeout,
+        screenshot: path.join(config.screenshotDir, `${ identifier }-desktop-original.jpeg`)
+      })
+
+      await reportService.updateProgress(identifier, 'Load origin desktop page... done')
+
+      console.timeEnd('Load origin desktop page')
+
+      console.log(result)
+
+      const location = await originDesktopPage.evaluate(() => ({
+        hostname: location.hostname,
+        protocol: location.protocol,
+        href: location.href
+      }))
+
+      const normalize = normalizeUrl(location.protocol, location.hostname)
+
+      state.url = location.href
+      state.desktopOriginal = result
+
+      await reportService.update(identifier, {
+        url: state.url,
+        desktopOriginalData: state.desktopOriginal
+      })
+      // collection <img> tags
+      const imgTags = await originDesktopPage.evaluate(() => {
+        const tags = document.querySelectorAll('img')
+
+        return Array.from(tags || []).map(
+          (tag) => ({
+            natural: {
+              width: tag.naturalWidth,
+              height: tag.naturalHeight
+            },
+            displayed: {
+              width: tag.clientWidth,
+              height: tag.clientHeight
+            },
+            src: tag.getAttribute('src')
+          })
+        )
+      })
+
+      // index imgTags
+      const imagesInHTML = imgTags.reduce(
+        (all, img) => {
+          if (!img.src) {
+            return all
+          }
+
+          const url = normalize(img.src.trim())
+
+          return {
+            ...all,
+            [url]: img
+          }
+        },
+        {}
+      )
+
+      // populate optimize urls
+      Object.values(state.images).forEach((image) => {
+        const url = image.url
+
+        const img = imagesInHTML[url] || {}
+
+        const { displayed } = img
+
+        state.images[url] = {
+          ...state.images[url],
+          ...img,
+          optimizedUrl: displayed ?
+            `${ config.optimizerEndpoint }/u?url=${ encodeURIComponent(url) }&w=${ displayed.width }&h=${ displayed.height }` :
+            `${ config.optimizerEndpoint }/u?url=${ encodeURIComponent(url) }`
+        }
+      })
+    } catch (e) {
+      throw e
+    } finally {
+      await originDesktopPage.close()
+
+      console.log('Origin desktop page closed')
+    }
+
+    // warm up optimized content
+    console.time('Warm up cache')
+
+    await reportService.updateProgress(identifier, 'Warm up cache...')
+
+    try {
+      await Promise.all(
+        Object.values(state.images).map(
+          ({ optimizedUrl }) => fetch(optimizedUrl)
+        )
+      )
+    } catch (e) {
+      // skip
+    } finally {
+      console.timeEnd('Warm up cache')
+
+      await reportService.updateProgress(identifier, 'Warm up cache... done')
+    }
+
+    const optimizedDesktopPage = await incognitoContext.newPage()
+
+    try {
+      // add request interception
+      await optimizedDesktopPage.setRequestInterception(true)
+
+      // handle requests
+      setRequestImage(optimizedDesktopPage, state.images)
+
+      // load optimized page
+      console.log('Load optimized desktop page')
+      console.time('Load optimized desktop page')
+
+      await reportService.updateProgress(identifier, 'Load optimized desktop page...')
+
+      const result = await loadPage(optimizedDesktopPage, {
+        url,
+        timeout,
+        screenshot: path.join(config.screenshotDir, `${ identifier }-desktop-optimized.jpeg`)
+      })
+
+      console.timeEnd('Load optimized desktop page')
+
+      await reportService.updateProgress(identifier, 'Load optimized desktop page... done')
+
+      console.log(result)
+
+      state.desktopOtimized = result
+
+      await reportService.update(identifier, {
+        desktopOptimizedData: state.desktopOtimized
+      })
+    } catch (e) {
+      throw e
+    } finally {
+      await optimizedDesktopPage.close()
+
+      console.log('Optimized desktop page closed')
+    }
+
+    const originalMobilePage = await incognitoContext.newPage()
+
+    try {
+      await reportService.updateProgress(identifier, 'Load origin mobile page...')
+      console.log('Load origin mobile page')
+      console.time('Load origin mobile page')
+
+      await originalMobilePage.setRequestInterception(true)
+
+      originalMobilePage.on('request', (request) => {
+        request.continue()
+      })
+
+      const originalMobileResult = await loadPage(originalMobilePage, {
+        url,
+        timeout,
+        screenshot: path.join(config.screenshotDir, `${ identifier }-mobile-original.jpeg`),
+        isMobile: true
+      })
+
+      await reportService.updateProgress(identifier, 'Load origin mobile page... done')
+
+      console.timeEnd('Load origin mobile page')
+
+      console.log(originalMobileResult)
+
+      state.mobileOriginal = originalMobileResult
+
+      await reportService.update(identifier, {
+        mobileOriginalData: state.mobileOriginal
+      })
+    } catch (e) {
+      throw e
+    } finally {
+      await originalMobilePage.close()
+    }
+    const optimizedMobilePage = await incognitoContext.newPage()
+
+    try {
+      await optimizedMobilePage.setRequestInterception(true)
+
+      setRequestImage(optimizedMobilePage, state.images)
+
+      // load mobile optimized page
+      console.log('Load optimized mobile page')
+      console.time('Load optimize mobile page')
+      await reportService.updateProgress(identifier, 'Load optimized mobile page...')
+
+      const resultMobileOtimized = await loadPage(optimizedMobilePage, {
+        url,
+        timeout,
+        screenshot: path.join(config.screenshotDir, `${ identifier }-mobile-optimized.jpeg`),
+        isMobile: true
+      })
+
+      console.timeEnd('Load optimize mobile page')
+
+      await reportService.updateProgress(identifier, 'Load optimized mobile page... done')
+
+      console.log(resultMobileOtimized)
+
+      state.mobileOptimized = resultMobileOtimized
+
+      await reportService.update(identifier, {
+        mobileOptimizedData: state.mobileOptimized
+      })
+    } catch (e) {
+      throw e
+    } finally {
+      await optimizedMobilePage.close()
+
+      console.log('Optimized mobile page closed')
+    }
+  } catch (e) {
+    throw e
+  } finally {
+    await browser.close()
+
+    console.log('Browser closed')
+  }
+}
+
+export default launchPuppeteer
---
+++ src/server/services/analyzer/load-page.js
@@ -0,0 +1,108 @@
+import delay from 'delay'
+import devices from 'puppeteer/DeviceDescriptors'
+import fs from 'fs-extra'
+import ms from 'ms'
+import path from 'path'
+
+const loadPage = async (page, params = {}) => {
+  const {
+    url,
+    screenshot,
+    width = 1440,
+    height = 900,
+    mode = 'load',
+    timeout = '5m',
+    isMobile = false
+  } = params
+
+  // await page._client.send('Performance.enable')
+  await page.setViewport({
+    width,
+    height,
+    isMobile,
+  })
+
+  const resources = {}
+
+  page._client.on('Network.dataReceived', (event) => {
+    const req = page._networkManager._requestIdToRequest.get(event.requestId)
+
+    if (!req) {
+      return
+    }
+
+    const url = req.url()
+
+    if (url.startsWith('data:')) {
+      return
+    }
+
+    const length = event.dataLength
+
+    if (!resources[url]) {
+      resources[url] = { size: 0 }
+    }
+
+    resources[url].size += length
+  })
+
+  if (url) {
+    if (isMobile) {
+      const iPhone = devices['iPhone 8']
+      await page.emulate(iPhone)
+    }
+
+    await page.goto(url, {
+      waitUntil: mode,
+      timeout: ms(timeout)
+    })
+  } else {
+    await page.reload({
+      waitUntil: mode,
+      timeout: timeout
+    })
+  }
+
+  if (screenshot) {
+    await fs.ensureDir(path.dirname(screenshot))
+
+    await delay(ms('1s'))
+
+    await page.screenshot({
+      path: screenshot
+      // fullPage: true
+    })
+  }
+
+  const performance = JSON.parse(await page.evaluate(
+    () => JSON.stringify(window.performance)
+  ))
+
+  const latestTiming = Object.entries(performance.timing).reduce(
+    (max, [ key, value ]) => max.value > value ? max : { key, value }, { value: 0 }
+  )
+
+  const downloadedBytes = Object.values(resources).reduce(
+    (sum, { size }) => sum + (size || 0), 0
+  )
+
+  const timeToFirstByte = performance.timing.responseStart - performance.timing.requestStart
+  const request = performance.timing.requestStart - performance.timing.connectEnd
+  const response = performance.timing.responseEnd - performance.timing.responseStart
+  const processing = performance.timing.loadEventStart - performance.timing.domLoading
+
+  // refer
+  // https://marketing.adobe.com/resources/help/en_US/sc/implement/performanceTiming.html
+  // https://developer.mozilla.org/en-US/docs/Web/API/Navigation_timing_API
+
+  return {
+    loadTime: latestTiming.value - performance.timing.navigationStart,
+    timeToFirstByte,
+    request,
+    response,
+    processing,
+    downloadedBytes
+  }
+}
+
+export default loadPage
