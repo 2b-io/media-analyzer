8307addcc5e678f9af7a29894d2bdea993b2b533
cuongtv <truongcuong90@gmail.com>
2019-02-21 18:10:28 +0700

    Add bar graph desktop optimized

--- src/server/controllers/report-detail.js
+++ src/server/controllers/report-detail.js
@@ -1,9 +1,12 @@
 import hash from '@emotion/hash'
 import bodyParser from 'body-parser'
+import fs from 'fs-extra'
 import { BAD_REQUEST, NOT_FOUND } from 'http-status-codes'
 import joi from 'joi'
 import ms from 'ms'
 import normalizeUrl from 'normalize-url'
+import path from 'path'
+import prettyMs from 'pretty-ms'
 import serializeError from 'serialize-error'
 
 import config from 'infrastructure/config'
@@ -31,10 +34,43 @@ export default {
         if (!report.finish) {
           return res.render('pages/report-detail', { report })
         }
+        const { log } = await fs.readJson(path.join(config.harDir, `${ identifier }-desktop-optimized.har`))
+
+        const requests = log.entries.map((element) => {
+          const {
+            timings: {
+              blocked,
+              dns,
+              connect,
+              send,
+              wait,
+              receive,
+              _queued: queued
+            }
+          } = element
+          return {
+            ...element,
+            blockedPercent: Math.max(0, ((blocked * 100) / log.pages[0].pageTimings.onLoad).toFixed(2)),
+            dnsPercent: Math.max(0, ((dns * 100) / log.pages[0].pageTimings.onLoad).toFixed(2)),
+            connectPercent: Math.max(0, ((connect * 100) / log.pages[0].pageTimings.onLoad).toFixed(2)),
+            sendPercent: Math.max(0, ((send * 100) / log.pages[0].pageTimings.onLoad).toFixed(2)),
+            waitPercent: Math.max(0.1, ((wait * 100) / log.pages[0].pageTimings.onLoad).toFixed(2)),
+            receivePercent: Math.max(0, ((receive * 100) / log.pages[0].pageTimings.onLoad).toFixed(2)),
+            totalTime: prettyMs(blocked + dns + connect + send + wait + receive),
+            queuedStart: ((Date.parse(element.startedDateTime) - Date.parse(log.pages[0].startedDateTime)) * 100) / log.pages[0].pageTimings.onLoad
+          }
+        })
+
+        const desktopOriginalHar = {
+          requests,
+          onLoad: log.pages[0].pageTimings.onLoad,
+          startTime: Date.parse(log.pages[0].startedDateTime)
+        }
 
         res.render('pages/report-detail', {
           report: {
-            ...report
+            ...report,
+            desktopOriginalHar
           }
         })
       } catch (e) {
