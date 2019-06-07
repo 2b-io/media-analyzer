4f85d640ce7927c66bcfe62743ec17cb5c9aa601
cuongtv <truongcuong90@gmail.com>
2019-06-07 17:50:24 +0700

    Format code

--- src/server/services/analyzer/lighthouse/format-data-lighthouse.js
+++ src/server/services/analyzer/lighthouse/format-data-lighthouse.js
@@ -3,19 +3,19 @@ import { auditStructData } from './lighthouse-struct-data'
  const formatDataLighthouse = (originalLighthouseData) => {
   let auditsData = Object.keys(originalLighthouseData.audits).map((element) => {
     if (auditStructData.indexOf(element) !== -1) {
-        let key = element.replace(/-([a-z])/g, (g) => g[1].toUpperCase())
-        let value = originalLighthouseData.audits[element]
+      let key = element.replace(/-([ a-z ])/g, (g) => g[ 1 ].toUpperCase())
+      let value = originalLighthouseData.audits[ element ]
 
-        return { [ key ]: value }
+      return { [ key ]: value }
     }
 
     return
   })
   .filter(Boolean)
-  .reduce( (ac, p) => {
-    let key = Object.keys(p)[0]
-    let value = Object.values(p)[0]
-    return {...ac,  [ key ]: value }
+  .reduce((arr, element) => {
+    let key = Object.keys(element)[ 0 ]
+    let value = Object.values(element)[ 0 ]
+    return { ...arr, [ key ]: value }
   }, {} )
 
   let {
