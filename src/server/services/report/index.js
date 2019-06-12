8566dd81e59cfeb8a69accafd22496310718b71b
cuongtv <truongcuong90@gmail.com>
2019-06-12 17:06:00 +0700

    Fix bug code merge from branch develop

--- src/server/services/report/index.js
+++ src/server/services/report/index.js
@@ -1,6 +1,9 @@
 import uuid from 'uuid'
 
 import Report from 'models/report'
+import ReportModel from 'models/report'
+
+import config from 'infrastructure/config'
 
 import { createWatcher } from './watcher'
 
@@ -15,8 +18,23 @@ const get = async (identifier) => {
   return Report.findOne({ identifier }).lean()
 }
 
+const list = async (page = 1) => {
+  const { pageNumberOfReports, reportPaginationStep } = config
+  const reports = await ReportModel.find().sort('-createdAt').limit(Number(pageNumberOfReports)).skip(Number(pageNumberOfReports * (page - 1)))
+  const reportsHit = await ReportModel.countDocuments()
+
+  return {
+    totalPage: Math.ceil(reportsHit / Number(pageNumberOfReports)),
+    currentPage: page,
+    reports,
+    reportPaginationStep
+  }
+}
+
+
 export default {
   create,
   createWatcher,
+  list,
   get
 }
